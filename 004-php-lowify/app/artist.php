847a786eb7de19cc87a2333bf80fc8b2d6364983
Louis <louis.ortega@coda-student.school>
2025-11-28 13:41:19 +0100

    Projet php

--- 004-php-lowify/app/artist.php
+++ 004-php-lowify/app/artist.php
@@ -4,15 +4,42 @@ require_once "inc/database.inc.php";
 $db = null;
 try{
     $db = new DatabaseManager(
-        " mysql:host=mysql;dbname=lowify;charset=utf8mb4",
+        "mysql:host=mysql;dbname=lowify;charset=utf8mb4",
         $username = "lowify",
         $password = "lowifypassword",
     );
 } catch (PDOException $ex) {
     echo "Erreur lors du chargement";
-}try{
-    $Artist = $db ->executeQuery("SELECT * FROM artist");
+}
+try{
+    $allArtists = $db ->executeQuery("SELECT * FROM artist");
 }catch (PDOException $ex) {
     echo "Erreur lors de la requête";
 }
-$search = $_GET['id'] ;
+
+$search = $_GET['id'] ?? 0;
+
+$allArtistsAsHTML = "";
+$iterator = 0;
+
+    try{
+        $Artist = $db ->executeQuery("SELECT * FROM artist");
+
+        $topSongs = $db->executeQuery("SELECT * FROM song ORDER BY note DESC LIMIT 5");
+
+        $album = $db ->executeQuery("SELECT * FROM album ORDER BY release_date DESC");
+    }catch (PDOException $ex) {
+        echo "Erreur lors de la requête";
+    }
+$html = <<<HTML
+
+HTML;
+$rawCSS =<<<CSS
+
+CSS;
+
+
+$page = new HTMLPage("lowify");
+$page ->addContent($html);
+$page ->addContent($rawCSS);
+echo $page->render();
--- 004-php-lowify/app/artists.php
+++ 004-php-lowify/app/artists.php
@@ -4,64 +4,70 @@ require_once "inc/database.inc.php";
 $db = null;
 try{
     $db = new DatabaseManager(
-        " mysql:host=mysql;dbname=lowify;charset=utf8mb4",
+        "mysql:host=mysql;dbname=lowify;charset=utf8mb4",
         $username = "lowify",
         $password = "lowifypassword",
     );
 } catch (PDOException $ex) {
-    echo "Erreur lors du chargement";
+    echo "Erreur lors du chargement".$ex->getMessage();
 }
+$allArtists = [];
 try{
     $allArtists = $db ->executeQuery("SELECT * FROM artist");
 }catch (PDOException $ex) {
     echo "Erreur lors de la requête";
 }
+
+$search = $_GET['id'] ?? 0;
+
 $allArtistsAsHTML = "";
 $iterator = 0;
 
 foreach ($allArtists as $artist) {
-    $artistName = $artist["artistName"];
-    $artistId = $artist["artistId"];
+    $artistName = $artist["name"];
+    $artistId = $artist["id"];
     $artistCover = $artist["cover"];
 
     if ($iterator % 5 == 0) {
-    $allArtistsAsHTML = '<div class="row mb-4">';
+        $allArtistsAsHTML = '<div class="row mb-4">';
     }
-    $artistsAsHTML .= <<<HTML
-            <div class="col-lg-3 col-md-6 mb-4">
-                <a href="artist.php?id=$artistId" class="text-decoration-none text-white">
-                    <div class="card h-100 bg-dark text-white border-dark shadow">
-                        <img src="$artistCover" class="card-img-top rounded-circle" alt="Image 1">
-                        <div class="card-body bg-secondary-subtle  text-white">
-                            <h5 class="card-title">$artistName</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-HTML;
+    $allArtistsAsHTML .= <<<HTML
+    <div class="col-lg-3 col-md-6 mb-4">
+        <a href="artists.php?id=$artistId" class="text-decoration-none text-white"></a>
+        <div class="card h-100 bg-dark text-white border-dark shadow">
+            <img src="$artistCover" class="card-img-top rounded-circle" alt="Image 1">
+                <div class="card-body bg-secondary-subtle  text-white">
+                    <h5>$artistName</h5>
+                </div>
+        </div>
+    </div>
+    
+    HTML;
     if ($iterator % 4 == 3) {
-        $artistsAsHTML .= '</div>';
+        $allArtistsAsHTML .= '</div>';
     }
 
     $iterator++;
-}
-$html = <<<HTML
-<div class="container bg-dark text-white p-4">
-        <a href="index.php" class="link text-white"> < Retour à l'accueil</a>
-
-    <h1 class="mb-4">Artistes</h1>
+}$html = <<<HTML
+    <style>
+        
+    </style>
     
-    <div>
-    {$artistsAsHTML}
+    <div class="container bg-dark text-white p-4">
+            <a href="index.php" class="link text-white"> < Retour à l'accueil</a>
+    
+        <h1 class="mb-4">Artistes</h1>
+        
+        <div>
+        {$allArtistsAsHTML}
+        </div>
     </div>
-</div>
-HTML;
-    $rawCSS =<<<CSS
-h1{
-color: red;
-}
-CSS;
+    HTML;
+        $rawCSS = <<<CSS
+    h1{
+    color: red;
+    }
+    CSS;
 
 
 
