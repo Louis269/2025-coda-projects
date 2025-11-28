dec2a1dcb23f43901fdcade5ae63916a752a2511
Louis <louis.ortega@coda-student.school>
2025-11-28 16:07:44 +0100

    Projet php

--- 004-php-lowify/app/errors.php
+++ 004-php-lowify/app/errors.php
@@ -1,7 +1,11 @@
 <?php
 require_once "inc/page.inc.php";
-$message = $_GET["message"] ?? 0;
-echo "<h1>$message</h1>";
-$html = <<<HTML
+$message = $_GET["message d'erreur"] ?? "Erreur inconnue";
 
+$html = <<<HTML
+<h1>$message</h1>
 HTML;
+$page = new HTMLPage("lowify");
+$page ->addContent($html);
+
+echo $page->render();
